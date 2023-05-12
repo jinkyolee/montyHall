014b7b9a5f2b8001bdf13ef5bf53adc847f2c3cd
jinkyolee <63771067+jinkyolee@users.noreply.github.com>
2023-05-12 20:10:34 +0100

    finished (jin)
    results are wrong!

--- src/Door.java
+++ src/Door.java
@@ -1,22 +1,11 @@
 public class Door {
-    private boolean hasPrize;
-    private boolean revealable;
-    private boolean choosable;
+    public boolean hasPrize;
+    public boolean revealable;
+    public boolean choosable;
 
     public Door(boolean hasPrize) {
         this.hasPrize = hasPrize;
         revealable = true;
-    }
-
-    public boolean getStat() {
-        return this.hasPrize;
-    }
-
-    public void unrevealable() {
-        this.revealable = false;
-    }
-
-    public void unchoosable() {
-        this.choosable = false;
+        choosable = true;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -2,48 +2,122 @@ import java.security.SecureRandom;
 import java.util.Scanner;
 
 public class Game {
-    private int prizeDoor;
-    private int chosenDoor;
-    private boolean change;
-    private Door[] doorList = new Door[3];
+    public int prizeDoor;
+    public int chosenDoor;
+    public boolean change;
+    public int iteration;
+    public Door[] doorList = new Door[3];
+    public static SecureRandom rand = new SecureRandom();
+    public static Scanner input = new Scanner(System.in);
+    public static int wins;
+    public static int losses;
 
     public Game() {
-        SecureRandom rand = new SecureRandom();
-
-        change = false;
-        prizeDoor = rand.nextInt(3);
-
-        doorList[prizeDoor].unrevealable();
-        for (int i = 0; i < 3; i++) {
-            doorList[i] = new Door((prizeDoor == i) ? true : false);
-        }
+        initGame();
     }
 
     public static void main(String[] args) {
         Game game = new Game();
-        SecureRandom rand = new SecureRandom(null);
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Will you change doors in this simulation (Y/N): ");
+        game.inputChoice();
+        game.inputIteration();
+
+        for (int i = game.iteration; i > 0; i--) {
+            game.runGame();
+        }
+
+        System.out.println("Wins: " + wins + "\nLosses: " + losses);
+    }
+
+    public void initGame() {
+        change = false;
+        prizeDoor = rand.nextInt(3);
+
+        // init doors
+        for (int i = 0; i < 3; i++) {
+            doorList[i] = new Door((prizeDoor == i) ? true : false);
+            // System.out.println("Door " + i + " initialized");
+        }
+
+        // System.out.println("Prize door is: " + prizeDoor);
+
+        // change revealability of door with prize
+        doorList[prizeDoor].revealable = false;
+
+        // System.out.println("Game initiated");
+    }
+
+    public void runGame() {
+        // choose door at random
+        this.chooseDoor();
+        this.revealDoor();
+        this.changeDoor();
+        this.gameResult();
+        this.initGame();
+    }
+
+    // get input for changing choice
+    public void inputChoice() {
+        System.out.print("Will you change doors in this simulation? (Y/N): ");
         String choiceStr = input.next().toLowerCase();
-        game.changeChoice(choiceStr.equals("y"));
-
-        game.chooseDoor(rand.nextInt(3));
+        this.change = choiceStr.equals("y");
     }
 
-    public void changeChoice(boolean choice) {
-        this.change = choice;
+    public void inputIteration() {
+        System.out.print("How many rounds should this session simulate? : ");
+        this.iteration = input.nextInt();
     }
 
-    public void chooseDoor(int doorNum) {
-        this.chosenDoor = doorNum;
+    public void editChoosable(int doorNum, boolean val) {
+        this.doorList[doorNum].choosable = val;
     }
 
+    public void chooseDoor() {
+        this.chosenDoor = rand.nextInt(3);
+        editChoosable(chosenDoor, false);
+        doorList[chosenDoor].revealable = false;
+
+        // System.out.println("Chosen door: " + chosenDoor);
+    }
+
+    // reveals door making one of empty doors unchoosable
     public void revealDoor() {
+        int revealNum = 0;
 
+        for (int i = 0; i < 3; i++) {
+            if (this.doorList[i].revealable) {
+                // System.out.println("Reveal door found at: " + i);
+                revealNum = i;
+                break;
+            }
+        }
+
+        doorList[revealNum].choosable = false;
+        // System.out.println("Revealed door: " + revealNum);
     }
 
-    public void changeDoor(boolean change) {
+    // change or maintains chosen door according to input value
+    public void changeDoor() {
+        if (this.change) {
+            int newDoor = 0;
 
+            for (int i = 0; i < 3; i++) {
+                if (this.doorList[i].choosable) {
+                    newDoor = i;
+                    break;
+                }
+            }
+
+            chosenDoor = newDoor;
+            // System.out.println("Chosen door changed to: " + chosenDoor);
+        }
+    }
+
+    public void gameResult() {
+        if (chosenDoor == prizeDoor) {
+            wins++;
+        } else {
+            losses++;
+        }
     }
 }
